d7f8c219571b3f6abc1a63b7e20cb2decee06f6e
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-05-16 21:07:07 -0700

    Set up list elements inside ul tag

--- public/app.js
+++ public/app.js
@@ -5,18 +5,29 @@ document.getElementById('filterForm').addEventListener('submit', async e => {
     const params = new URLSearchParams()
 
     new FormData(form).forEach((value, key) => {
-        params.append( key, value)
+        params.append(key, value)
     })
 
     //console.log(params.toString())
 
     try {
         const res = await fetch('/exercises?' + params.toString())
-        console.log('/exercises?' + params.toString())
         if (!res.ok) throw new Error(`HTTP ${res.status}`)
-        const exercises = await res.json()
-        // console.log(exercises)
+        const exercises = await res.json() // format response as JSON
+
+        // Set up list elements inside ul tag
+        const ul = document.getElementById('results')
+        if (exercises.length === 0) {
+            ul.innerHTML = '<li>No exercises found.</li>'
+        } else {
+            ul.innerHTML = exercises.map(exercises => {
+                return `<li>${exercises.name}</li>`
+            }).join('')
+        }
+
     } catch (err) {
         console.error('Error fetching exercises:', err)
+        document.getElementById('results').innerHTML =
+            '<li style="color:red">Failed to load exercises.</li>'
     }
 })
