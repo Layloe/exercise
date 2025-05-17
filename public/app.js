02ab8de517bb74a19b93e8a33a3a9629c58f0c7d
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-05-16 20:54:20 -0700

    testing from server

--- public/app.js
+++ public/app.js
@@ -1,12 +1,21 @@
 // Manually configure from to fetch data and format results as HTML
 document.getElementById('filterForm').addEventListener('submit', async e => {
     e.preventDefault()
+    const form = e.target
+    const params = new URLSearchParams()
+
+    new FormData(form).forEach((value, key) => {
+        params.append( key, value)
+    })
+
+    //console.log(params.toString())
 
     try {
-        const res = await fetch('/exercises')
+        const res = await fetch('/exercises?' + params.toString())
+        console.log('/exercises?' + params.toString())
         if (!res.ok) throw new Error(`HTTP ${res.status}`)
         const exercises = await res.json()
-        console.log(exercises)
+        // console.log(exercises)
     } catch (err) {
         console.error('Error fetching exercises:', err)
     }
